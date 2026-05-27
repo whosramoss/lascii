3d5d1e064b832e6284c880059d4be16dd6312185
Gabriel Ramos <whosramoss@gmail.com>
2026-05-27 20:42:56 -0300

    build: configure package entrypoints and exports

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import LasciiImageEffect from "./core/effects/LasciiImageEffect.js";
+import LasciiTextEffect from "./core/effects/LasciiTextEffect.js";
+import { autoInitDom, initDom } from "./adapters/dom/initDom.js";
+
+autoInitDom();
+
+export { LasciiImageEffect, LasciiTextEffect, initDom as init, autoInitDom };
+export default {
+  LasciiImageEffect,
+  LasciiTextEffect,
+  init: initDom,
+  autoInitDom,
+};
